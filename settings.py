a9ebea4b79762ea5e72034129aa963d27a60afe0
gianniskts <johnkotsias@hotmail.gr>
2022-07-17 22:19:00 +0300

    default settings

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 mysql_host =  '127.0.0.1' 
 mysql_port = 3306  #this must be integer (this is wrong:'3306')
 mysql_user = 'root'   #you must replace this value with your own settings
-mysql_passwd = 'sasa' #you must replace this value with your own settings 
+mysql_passwd = 'your_password' #you must replace this value with your own settings 
 mysql_schema = 'flights' #the database name, replace with your own value
 
 # Webserver Settings
